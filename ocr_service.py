311e72e44f16bd5bb985d1a79b9a542e0652142e
phattai0873 <nguyenhuynhphattai12cb6@gmail.com>
2026-02-02 01:37:54 +0700

    fix: remove use_gpu argument for paddleocr

--- ocr_service.py
+++ ocr_service.py
@@ -15,7 +15,6 @@ class OCRService:
         self.ocr = PaddleOCR(
             use_angle_cls=True,
             lang='vi',
-            use_gpu=False,
         )
         self.poppler_path = poppler_path
 
